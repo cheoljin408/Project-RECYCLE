7912d99b30851376eaea581faa0e2455daee419f
JunYoung <jun094@naver.com>
2019-08-15 13:29:12 +0900

    작품보기 page - 카테고리 UI 변경

--- public/javascripts/find.js
+++ public/javascripts/find.js
@@ -9,56 +9,6 @@ $('.row button').click(function() {
     $(this).css("border", "1px #d7dede solid");
   }
 });
-//category request
-$('.buy').children('button').click(function(){
-  var html = "";
-  var state = $('#rent').text();
-  $.ajax({
-    type: 'POST',
-    url: '/find',
-    async: false,
-    data: {
-              state : state
-    },
-    success: function(data) {
-      if (data != null) {
-        for (var i = 3; i < 20; i++) {
-          var category = data[i]['category'];
-          var local = data[i]['local'];
-          var state = data[i]['state'];
-          var title = data[i]['title'];
-          var user = data[i]['user'];
-          var price = data[i]['price'];
-          var img = data[i]['img'];
-
-
-
-          var plus = `<div class="paper">
-                        <div class="paper-holder">
-                          <a><img width="225" src="${img}" /></a>
-                        </div>
-                        <div class="paper-description">
-                          <p id='title'>${title}</p>
-                          <p id="userId">${user}</p>
-                        </div>
-                        <div class="paper-content">
-                          <span id="price">${price}원</span>
-                          <span class="paper-state">
-                            <span id="state_${i}" style="display:none">${state}</span>
-                            <span class="state1" id="state1_${i}">렌탈</span>
-                            <span class="state2" id="state2_${i}">구매</span>
-                          </span>
-                        </div>
-                      </div>`;
-          html += plus;
-
-        }
-        document.getElementById('masonry_container').innerHTML = html;
-      }
-    }
-  });
-});
-
 
 //masonry
 $('#masonry_container').imagesLoaded(function() {
