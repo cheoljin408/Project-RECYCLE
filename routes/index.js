7941c5b0828a792d32fdbd4ae9258f895e2221f7
wJ <mingwoojin@naver.com>
2019-08-20 19:05:54 +0900

    비밀번호 변경 전 이전 비밀번호 확인

--- routes/index.js
+++ routes/index.js
@@ -143,6 +143,11 @@ router.get('/profile/edit', function(req, res){
   res.render('profileEdit');
 })
 
+/* GET passwordChange page */
+router.get('/profile/password/change', function(req, res){
+  res.render('pwChange');
+})
+
 router.post('/upload', upload.single('userFile'), function(req, res){
   //res.send('Uploaded! : '+req.file); // object를 리턴함
   console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
